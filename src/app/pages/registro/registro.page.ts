f4b455dfa200234bfeb84daeb37c9425e8f88c97
waka2022 <infomarcion.waka@gmail.com>
2022-03-03 13:27:09 -0500

    siuuuuuuuuuuu x3

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -17,13 +17,12 @@ export class RegistroPage implements OnInit {
   }
 
 
-
   users = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(4)]),
     last_name: new FormControl('', [Validators.required, Validators.minLength(4)]),
     email: new FormControl('', [Validators.required, Validators.pattern('[A-Za-z0-9._%+-]{2,}@[a-zA-Z-_.]{2,}[.]{1}[a-zA-Z]{2,}')]),
     password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$')]),
-    passconf: new FormControl('', Validators.required)
+  
     
   });
 
