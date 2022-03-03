42fea7fff1045ffe4951168a44eca88fa14527f5
SamuelCanoRomero <bscano67@misena.edu.co>
2022-03-03 17:24:22 -0500

    boton eliminar perfil

--- src/app/pages/PP/perfil-parqueadero/perfil-parqueadero.page.ts
+++ src/app/pages/PP/perfil-parqueadero/perfil-parqueadero.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular'
 
 @Component({
   selector: 'app-perfil-parqueadero',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilParqueaderoPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Eliminar la cuenta',
+      subHeader: 'La eliminación de cuenta es definitiva Al eliminarla',
+      message: 'tu cuenta de WAKA y toda la información tambien se eliminará',
+      buttons: [
+        {
+          text: 'Cancelar',
+          cssClass: 'btn1',
+          handler: () => {
+            console.log('Cancel');
+          }
+        }, {
+          text: 'Confirmar',
+          cssClass: 'btn2',
+          handler: () => {
+            console.log('Confirm');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
